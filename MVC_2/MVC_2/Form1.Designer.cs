834204f14295784ac1ea9d0f4f8f44f3fd30b635
khusamov Albert <khusamov01@mail.ru>
2020-11-25 00:07:46 +0500

    v2.1
    Приложение реализовано с идеей MVC

--- MVC_2/MVC_2/Form1.Designer.cs
+++ MVC_2/MVC_2/Form1.Designer.cs
@@ -28,13 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.trackBar = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.ForeColor = System.Drawing.Color.SteelBlue;
+            this.progressBar.Location = new System.Drawing.Point(208, 91);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(209, 23);
+            this.progressBar.Step = 1;
+            this.progressBar.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.PowderBlue;
+            this.textBox1.ForeColor = System.Drawing.Color.Navy;
+            this.textBox1.Location = new System.Drawing.Point(41, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.PowderBlue;
+            this.textBox2.ForeColor = System.Drawing.Color.Navy;
+            this.textBox2.Location = new System.Drawing.Point(433, 35);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(150, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+            // 
+            // trackBar
+            // 
+            this.trackBar.Location = new System.Drawing.Point(194, 187);
+            this.trackBar.Maximum = 100;
+            this.trackBar.Name = "trackBar";
+            this.trackBar.Size = new System.Drawing.Size(237, 45);
+            this.trackBar.TabIndex = 3;
+            this.trackBar.Scroll += new System.EventHandler(this.trackBar_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Azure;
+            this.ClientSize = new System.Drawing.Size(630, 362);
+            this.Controls.Add(this.trackBar);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(646, 401);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(646, 401);
+            this.Name = "Form1";
+            this.Text = "MVC_2";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TrackBar trackBar;
     }
 }
 
--- MVC_2/MVC_2/Form1.cs
+++ MVC_2/MVC_2/Form1.cs
@@ -12,9 +12,57 @@ namespace MVC_2
 {
     public partial class Form1 : Form
     {
+        MVC model;
         public Form1()
         {
             InitializeComponent();
+            model = new MVC();
+            model.observers += new System.EventHandler(this.updateFromMVC);
+        }
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                model.setValue(textBox1, textBox2);
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                model.setValue(textBox2, textBox1);
+        }
+        private void trackBar_Scroll(object sender, EventArgs e)
+        {
+            model.setValue(trackBar, textBox1, textBox2);
+        }
+        public void updateFromMVC(object sender, EventArgs e)
+        {
+            progressBar.Value = model.getValue();
+            trackBar.Value = model.getValue();
+        }
+        public class MVC
+        {
+            private int value;
+            public System.EventHandler observers;
+            public void setValue(TextBox T1, TextBox T2)
+            {
+                value = Convert.ToInt32(T1.Text);
+                if (T1.Name == "textBox1")
+                    T2.Text = (value + 1).ToString();
+                else
+                    T2.Text = (value - 1).ToString();
+                observers.Invoke(this, null);
+            }
+            public void setValue(TrackBar tr, TextBox T1, TextBox T2)
+            {
+                value = tr.Value;
+                T1.Text = value.ToString();
+                T2.Text = (value + 1).ToString();
+                observers.Invoke(this, null);
+            }
+            public int getValue()
+            {
+                return value;
+            }
         }
     }
 }
